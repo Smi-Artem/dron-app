326e54f2b1110409182bb0d104e186eb7b335992
Artem <smi-artem6591@mail.ru>
2026-07-11 21:11:31 +0700

    Update map_widget.py

--- ui/map_widget.py
+++ ui/map_widget.py
@@ -1,75 +1,60 @@
-
-
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
 
-
-
 class map_general(QWebEngineView):
-
-    def __init__(self):
+    def __init__(self, lat=55.355198, lon=86.086847, zoom=15):
         super().__init__()
 
-        self.setHtml("""
+        self.lat = lat
+        self.lon = lon
+        self.zoom = zoom
+
+        self._load_map()
+
+    def _load_map(self):
+        html_content = f"""
+        <!DOCTYPE html>
         <html>
-        <body style="background-color:yellow;">
-        <h1> КАРТА </h1>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link rel="stylesheet"
+                  href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+
+            <style>
+                html, body, #map {{
+                    width: 100%;
+                    height: 100%;
+                    margin: 0;
+                    padding: 0;
+                }}
+            </style>
+        </head>
+
+        <body>
+            <div id="map"></div>
+
+            <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+            <script>
+                var map = L.map('map').setView([{self.lat}, {self.lon}], {self.zoom});
+
+                L.tileLayer('https://tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
+                    maxZoom: 19,
+                    attribution: 'OpenStreetMap contributors'
+                }}).addTo(map);
+
+                L.marker([{self.lat}, {self.lon}]).addTo(map)
+                    .bindPopup("Вы здесь")
+                    .openPopup();
+
+                setTimeout(function() {{
+                    map.invalidateSize();
+                }}, 300);
+            </script>
         </body>
         </html>
-        
-        """)
+        """
 
-        # self.setHtml("""
-        # <!DOCTYPE html>
-        # <html>
-        # <head>
-        # <link rel = "stylesheet"
-        # href = "https://unpkg.com/leaflet/dist/leaflet.css"/>
-        # <script src = "https://unpkg.com/leaflet/dist/leaflet.js">
-        # </script>
-        #
-        # <style>
-        #
-        # html, body, #map{
-        #     height: 100%;
-        #     margin: 0;
-        # }
-        #
-        # </style>
-        # </head>
-        #
-        # <body>
-        # <div id="map"></div>
-        #
-        # <script>
-        #
-        # let lat = 55.756;
-        # let lon = 37.618;
-        #
-        # let map = L.map("map").setView([lat, lon], 15);
-        # L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")}",
-        # {maxZoom: 19}).addTo(map);
-        #
-        # let marker = L.circleMarker(
-        # [lat, lon],
-        # {radius: 10,
-        # color = "red",
-        # fillColor = "red",
-        # fillOpacity = 1,
-        # }
-        # ).addTo(map);
-        #
-        # function updateDrone(lat, lon)
-        # {
-        #     map.setView(
-        #     [lat, lon],
-        #     15);
-        #
-        #     marker.setLatLng([lat, lon]);
-        # }
-        #
-        # </script>
-        # </body>
-        # </html>
-        #
-        # """)
+        self.setHtml(html_content, QUrl("https://tile.openstreetmap.org/"))
